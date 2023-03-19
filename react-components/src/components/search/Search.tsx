db4b51f89c294745dbced534b60b9f94d786d480
Darya Suravtsova <darblnkapm@gmail.com>
2023-03-20 01:22:07 +0300

    feat: add localStorage logic

--- react-components/src/components/search/Search.tsx
+++ react-components/src/components/search/Search.tsx
@@ -1,9 +1,21 @@
 import styles from './Search.module.css';
 import React from 'react';
 
-class Search extends React.Component {
+type SearchProps = {
+  onInput: (e: React.KeyboardEvent<HTMLInputElement>) => void | undefined;
+  value: string;
+};
+
+class Search extends React.Component<SearchProps> {
   render() {
-    return <input className={styles.container} placeholder="Search..." />;
+    return (
+      <input
+        className={styles.container}
+        placeholder="Search..."
+        onInput={this.props.onInput}
+        defaultValue={this.props.value}
+      />
+    );
   }
 }
 
--- react-components/src/routes/main/Main.tsx
+++ react-components/src/routes/main/Main.tsx
@@ -4,15 +4,45 @@ import Product from '../../components/product/Product';
 import Search from '../../components/search/Search';
 import { productType } from '../../types';
 
-export type MainProps = {
+type MainProps = {
   products: productType[];
 };
 
-class Main extends React.Component<MainProps> {
+type MainState = {
+  searchValue: string;
+};
+
+class Main extends React.Component<MainProps, MainState> {
+  constructor(props: MainProps) {
+    super(props);
+    this.setSearchValue = this.setSearchValue.bind(this);
+
+    this.state = {
+      searchValue: localStorage.getItem('searchValue') || '',
+    };
+  }
+
+  setSearchValue(): void {
+    localStorage.setItem('searchValue', this.state.searchValue);
+  }
+
+  componentDidMount() {
+    window.addEventListener('beforeunload', this.setSearchValue);
+  }
+
+  componentWillUnmount() {
+    this.setSearchValue();
+    window.removeEventListener('beforeunload', this.setSearchValue);
+  }
+
+  handleSearchFilter = (e: React.KeyboardEvent<HTMLInputElement>): void => {
+    this.setState({ searchValue: (e.target as HTMLInputElement).value });
+  };
+
   render() {
     return (
       <div>
-        <Search />
+        <Search onInput={this.handleSearchFilter} value={this.state.searchValue} />
         <div className={styles.products}>
           {this.props.products.map((product) => (
             <Product
