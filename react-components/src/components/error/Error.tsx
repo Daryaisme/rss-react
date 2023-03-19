b536ee807761b7a663678265b715f774db8679f5
Darya Suravtsova <darblnkapm@gmail.com>
2023-03-19 22:20:10 +0300

    feat: add Error component markup

--- react-components/src/components/error/Error.tsx
+++ react-components/src/components/error/Error.tsx
@@ -1,9 +1,19 @@
-//import styles from 'Error.module.css';
+import styles from './Error.module.css';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Error extends React.Component {
   render() {
-    return <div>dmkmdsed</div>;
+    return (
+      <div className={styles.container}>
+        <div>
+          <p className={styles.digital}>404</p>
+          <p className={styles.link}>
+            Back to <Link to="/">main</Link>
+          </p>
+        </div>
+      </div>
+    );
   }
 }
 
